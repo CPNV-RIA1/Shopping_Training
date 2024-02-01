eab6b7a84a0f344bfbf35b21f3b01af9df8d6fa6
EliottJaquierEdu <eliott.jaquier@eduvaud.ch>
2024-02-01 11:10:58 +0100

    refactor : having items passed to when ensuring

--- src/Cart/Cart.js
+++ src/Cart/Cart.js
@@ -11,25 +11,25 @@ module.exports = class Cart {
         this.#items = items;
     }
     get items(){
-        this.#ensureItemsNotEmpty()
+        this.#ensureItemsNotEmpty(this.#items)
         return this.#items;
     }
     get total(){
-        this.#ensureItemsNotEmpty()
+        this.#ensureItemsNotEmpty(this.#items)
         return this.#items.reduce((accumulator, item) => accumulator + item.total, 0);
     }
     count(distinct = false){
-        this.#ensureItemsNotEmpty()
+        this.#ensureItemsNotEmpty(this.#items)
         return distinct ? this.#items.length : this.#items.reduce((accumulator, item) => accumulator + item.quantity, 0);
     }
     //endregion public methods
 
     //region private methods
-    #ensureItemsNotEmpty(){
-        if(this.#areItemsEmpty()) throw new EmptyCartException();
+    #ensureItemsNotEmpty(items){
+        if(this.#areItemsEmpty(items)) throw new EmptyCartException();
     }
-    #areItemsEmpty(){
-        return !this.#items || this.#items.length < 1;
+    #areItemsEmpty(items){
+        return !items || items < 1;
     }
     //endregion private methods
 }
